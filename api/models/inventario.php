94d62db3598779c3900de43157ad54b7ed5cfa66
2Miguel4 <a.alexrivas24@gmail.com>
2022-06-27 13:56:14 -0600

    Declaracion de variables
    Declaramos las variables de la base de datos

---
+++ api/models/inventario.php
@@ -0,0 +1,16 @@
+<?php
+
+class inventario extends Validator
+{
+    // Declaración de atributos (propiedades) de  la base de datos.
+    private $id_producto = null;
+    private $nombre_prodroducto = null;
+    private $cantidad_prodroducto = null;
+    private $descripcion_producto = null;
+    private $estado_producto = null;
+    private $precio_unidad = null;
+    private $id_marca = null;
+    private $id_usuario = null;
+
+}
+?>
