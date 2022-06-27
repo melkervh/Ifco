010412d9ba8aa78615d6332bd70d0c394794134a
Yeck-Sang <jorgesecas@gmail.com>
2022-06-27 07:24:31 -0600

    cambio de servidor

--- api/helpers/database.php
+++ api/helpers/database.php
@@ -16,9 +16,9 @@ class Database
     {
         // Credenciales para establecer la conexión con la base de datos.
         $server = 'localhost';
-        $database = 'ifcobase';
+        $database = 'ifco';
         $username = 'postgres';
-        $password = '1234';
+        $password = 'admin';
 
         // Se crea la conexión mediante la extensión PDO y el controlador para PostgreSQL.
         self::$connection = new PDO('pgsql:host=' . $server . ';dbname=' . $database . ';port=5432', $username, $password);
