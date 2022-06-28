38a2c321128116d1cfa9bd34dfb64aad4fb6203f
2Miguel4 <a.alexrivas24@gmail.com>
2022-06-28 08:54:57 -0600

    Terminamos el models de productos

--- api/models/productos.php
+++ api/models/productos.php
@@ -143,7 +143,6 @@ class inventario extends Validator
     *   Métodos para realizar las operaciones SCRUD (search, create, read, update, delete).
     */
 
-    /*Buscador de productos*/
     public function searchRows($value)
     {
         $sql = 'SELECT id_producto, nombre_prodroducto, cantidad_prodroducto, descripcion_producto, estado_producto, precio_unidad
@@ -157,9 +156,49 @@ class inventario extends Validator
     /*Crear un producto nuevo*/
     public function createRow()
     {
-        $sql = 'INSERT INTO productos(nombre_prodroducto, cantidad_prodroducto, descripcion_producto, estado_producto, precio_unidad, id_marca, id_usuario)
+        $sql = 'INSERT INTO producto(nombre_prodroducto, cantidad_prodroducto, descripcion_producto, estado_producto, precio_unidad, id_marca, id_usuario)
                 VALUES(?, ?, ?, ?, ?, ?, ?)';
         $params = array($this->nombre_prodroducto, $this->cantidad_prodroducto, $this->descripcion_producto,  $this->estadestado_producto, $this->precio_unidad,  $this->id_marca, $_SESSION['id_usuario']);
         return Database::executeRow($sql, $params);
     }
+
+    /*Buscar barios productos*/
+    public function readAll()
+    {
+        $sql = 'SELECT id_producto, nombre_prodroducto, cantidad_prodroducto, descripcion_producto, estado_producto, precio_unidad
+                FROM producto INNER JOIN marca USING(id_marca)
+                ORDER BY nombre_prodroducto';
+        $params = null;
+        return Database::getRows($sql, $params);
+    }
+
+    /*Buscar un producto*/
+    public function readOne()
+    {
+        $sql = 'SELECT nombre_prodroducto, cantidad_prodroducto, descripcion_producto, estado_producto, precio_unidad, id_marca
+                FROM productos
+                WHERE id_producto = ?';
+        $params = array($this->id_producto);
+        return Database::getRow($sql, $params);
+    }
+
+    /*Modificar producto*/
+    public function updateRow()
+    {
+        $sql = 'UPDATE producto
+                SET  nombre_prodroducto = ?, descripcion_producto = ?, precio_unidad = ?, estado_producto = ?, id_marca = ?
+                WHERE id_producto = ?';
+        $params = array($this->nombre_prodroducto, $this->descripcion_producto, $this->precio_unidad, $this->estadestado_producto, $this->id_marca);
+        return Database::executeRow($sql, $params);
+    }
+
+    /*Borrar un producto*/
+    public function deleteRow()
+    {
+        $sql = 'DELETE FROM producto
+                WHERE id_producto = ?';
+        $params = array($this->id_producto);
+        return Database::executeRow($sql, $params);
+    }
+}
 ?>
