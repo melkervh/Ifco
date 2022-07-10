19e40d64eb4ac59a9e8cf87ea4871954a1b9d112
melkervh <100394865+melkervh@users.noreply.github.com>
2022-07-10 17:04:43 -0600

    base

--- api/helpers/database.php
+++ api/helpers/database.php
@@ -18,7 +18,7 @@ class Database
         $server = 'localhost';
         $database = 'IFCO';
         $username = 'postgres';
-        $password = '';
+        $password = '1234a';
 
         // Se crea la conexión mediante la extensión PDO y el controlador para PostgreSQL.
         self::$connection = new PDO('pgsql:host=' . $server . ';dbname=' . $database . ';port=5432', $username, $password);
