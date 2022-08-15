2c256004cbbfe2d88ee3aac03dac98e907fed3ea
melkervh <jorgesecas@gmail.com>
2022-08-15 10:02:59 -0600

    vista estadisticas
    se creo la vista de la pagina para los reportes

--- controllers/acounst.js
+++ controllers/acounst.js
@@ -69,6 +69,9 @@ document.addEventListener('DOMContentLoaded', function () {
                                     </ul>
                                 </li>
                                 <li class="nav-item">
+                                <a class="nav-link" href="estadisticas.html">Estadisticas</a>
+                            </li>
+                                <li class="nav-item">
                                         <a onclick="logOut()" class="nav-link">Cerrar sesión</a>
                                     </li>
                             </ul>
