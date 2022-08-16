e7ec2ca45b054db0b37788827fe7250eb118d02d
2Miguel4 <a.alexrivas24@gmail.com>
2022-08-16 11:11:42 -0600

    Boton para el reporte de EstadoProducto
    se hace funcionar el boton solo falta hacer el codigo y la plantilla para el reporte

--- controllers/estadisticas.js
+++ controllers/estadisticas.js
@@ -1,13 +1,21 @@
 // Constantes para establecer las rutas y parámetros de comunicación con la API.
 //Parametros para el reposte de productos
+//Constantes para el reporte del estado de los productos 
 const API_PRODUCTOS = SERVER + 'Actions/productos.php?action=';
 const ENDPOINT_CATEGORIAS = SERVER + 'Actions/marcas.php?action=readAll';
-
 //Se inicia el codigo para la funcion del reporte para los productos y sus marcas.
 //Esta misma funcion nos permite hacer funcionar el boton para abrir el reporte.
 function openReporteProduc() {
     // Se establece la ruta del reporte en el servidor.
     let url = SERVER + 'reports/reporte_producto.php';
+    // Se abre el reporte en una nueva pestaña del navegador web.
+    window.open(url);
+}
+//Se inicia el codigo para la funcion del reporte de los estados por productos.
+//Esta misma funcion nos permite hacer funcionar el boton para abrir el reporte.
+function openReporteEstado() {
+    // Se establece la ruta del reporte en el servidor.
+    let url = SERVER + 'reports/reporte_estadopro.php';
     // Se abre el reporte en una nueva pestaña del navegador web.
     window.open(url);
 }
