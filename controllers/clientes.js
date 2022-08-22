c11dbfbd6809d4e4e9832fe599f139c58c1f19b0
2Miguel4 <a.alexrivas24@gmail.com>
2022-08-22 11:24:59 -0600

    boton parametrizado
    boton para el reporte parametrizado

--- controllers/clientes.js
+++ controllers/clientes.js
@@ -12,6 +12,7 @@ function fillTable(dataset) {
     // Se recorre el conjunto de registros (dataset) fila por fila a través del objeto row.
     dataset.map(function (row) {
         // Se crean y concatenan las filas de la tabla con los datos de cada registro.
+        //En la linea 29, se llama a la funcion para que se abra una nueva ventana la darle clic al icono
         content += `
         <tr>
         <td>${row.nombre_cli}</td>
@@ -25,7 +26,7 @@ function fillTable(dataset) {
         <a onclick="openDelete(${row.id_cliente})">
             <i class="fa-solid fa-trash-can"></i>
         </a>
-        <a onclick="openDelete(${row.id_cliente})">
+        <a  onclick="openReporteInfor()"(${row.id_cliente})">
         <i class="fa-solid fa-file-pdf"></i>
         </a>
     </td>
@@ -36,4 +37,12 @@ function fillTable(dataset) {
 
     // Se agregan las filas al cuerpo de la tabla mediante su id para mostrar los registros.
     document.getElementById('clientes-tabla').innerHTML = content;
+}
+//Se inicia el codigo para la funcion del reporte parametrizado del cliente y su respectivo departamento.
+//Esta misma funcion nos permite hacer funcionar el boton para abrir el reporte.
+function openReporteInfor() {
+    // Se establece la ruta del reporte en el servidor.
+    let url = SERVER + 'reports/reporte_ubicacion.php';
+    // Se abre el reporte en una nueva pestaña del navegador web.
+    window.open(url);
 }
