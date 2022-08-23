09819fc5241a9c27f38c28db2625cc4825ecba1b
2Miguel4 <a.alexrivas24@gmail.com>
2022-08-22 21:05:35 -0600

    reportes de:
     usuario/ departamento y productos/marcas

--- api/helpers/database.php
+++ api/helpers/database.php
@@ -16,7 +16,7 @@ class Database
     {
         // Credenciales para establecer la conexión con la base de datos.
         $server = 'localhost';
-        $database = 'IFCO';
+        $database = 'ifco';
         $username = 'postgres';
         $password = '1234';
 
