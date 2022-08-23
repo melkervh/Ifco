b1ffbd47f61f60289687d2b322468b49e5f2c3ae
melkervh <jorgesecas@gmail.com>
2022-08-23 07:43:49 -0600

    cambio

--- api/helpers/database.php
+++ api/helpers/database.php
@@ -16,7 +16,7 @@ class Database
     {
         // Credenciales para establecer la conexión con la base de datos.
         $server = 'localhost';
-        $database = 'ifco';
+        $database = 'IFCO';
         $username = 'postgres';
         $password = '1234';
 
